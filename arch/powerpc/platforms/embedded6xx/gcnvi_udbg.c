7ff4f7b555b13501fdf39032b3c6c830d7334882
techflashYT <officialTechflashYT@gmail.com>
2026-07-26 23:15:34 -0500

    powerpc: gcnvi_udbg: fix license header

--- arch/powerpc/platforms/embedded6xx/gcnvi_udbg.c
+++ arch/powerpc/platforms/embedded6xx/gcnvi_udbg.c
@@ -1,7 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * arch/powerpc/platforms/embedded6xx/gcnvi_udbg.c
- *
  * Nintendo GameCube/Wii framebuffer udbg output support.
  * Copyright (C) 2008-2009 The GameCube Linux Team
  * Copyright (C) 2008,2009 Albert Herranz
