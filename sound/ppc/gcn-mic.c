7e7c8f8a1f2985bf767d0fc1acc0924b18813c84
techflashYT <officialTechflashYT@gmail.com>
2026-07-26 22:33:55 -0500

    sound: ppc: gcn-mic: fix license/copyright header

--- sound/ppc/gcn-mic.c
+++ sound/ppc/gcn-mic.c
@@ -1,15 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * sound/ppc/gcn-mic.c
- *
  * Nintendo Microphone (DOL-022) driver
  * Copyright (C) 2006-2009 The GameCube Linux Team
  * Copyright (C) 2006,2007,2008,2009 Albert Herranz
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
+ * Copyright (C) 2026 Michael "Techflash" Garofalo
  */
 
 #define MIC_DEBUG
